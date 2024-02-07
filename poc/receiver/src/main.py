f3dc30ddd7aa82af3c20e0837000856969965f19
Sebastiaan Bij <sebastiaan.bij@vodafoneziggo.com>
2024-02-07 14:09:18 +0100

    Added provisioning

--- poc/receiver/src/main.py
+++ poc/receiver/src/main.py
@@ -1,20 +1,32 @@
 # Imports
 from socket import socket, AF_INET, SOCK_DGRAM
 from datetime import datetime
+from sys import argv
 from numpy import frombuffer, uint16
 
 # Variables
 udpServer = socket(AF_INET, SOCK_DGRAM)
 received = 0
 start = datetime.now()
+count = argv[1]
+index = {}
 
 # Main
 if __name__ == "__main__":
     try:
         udpServer.bind(("<HOST>", "<PORT(int)>"))
 
+        while len(index) != count:
+            raw_data, ip = udpServer.recvfrom(3200)
+            matrix = frombuffer(raw_data, uint16)
+
+            for cell in matrix:
+                if cell > 2000:
+                    index.update(ip)
+
         while True:
             try:
+                # TODO index from and send onward
                 matrix = frombuffer(
                     udpServer.recv(3200),
                     uint16
